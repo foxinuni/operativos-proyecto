2953fca6d2c5b5379ba4aacc5fdb3ddababf7f1c
Laura Mendieta <laumendieta06@gmail.com>
2024-11-11 15:36:08 -0500

    publisher already works

---
+++ src/publisher.c
@@ -0,0 +1,50 @@
+#include "publisher.h"
+
+void start_publisher(int argc, char* argv[]) {
+    char pipePSC[255];
+    char file[255];
+    int timeN;
+
+    flags_t* parser = flags_create();
+    flags_string(parser, pipePSC, 'p', "/tmp/pipePSC");
+    flags_string(parser, file, 'f', "news.txt");
+    flags_int(parser, &timeN, 't', 1);
+
+    flags_parse(parser, argc, argv);
+    flags_destroy(parser);
+
+    flog(LOG_INFO, "Publisher initialized with pipe: %s, file: %s, time interval: %d seconds\n", pipePSC, file, timeN);
+
+    FILE* fp = fopen(file, "r");
+    if (!fp) {
+        flog(LOG_ERROR, "Failed to open the news file: %s\n", file);
+        exit(EXIT_FAILURE);
+    }
+
+    int fd = open(pipePSC, O_WRONLY);
+    if (fd == -1) {
+        perror("open");
+        flog(LOG_ERROR, "Failed to open the pipe: %s\n", pipePSC);
+        fclose(fp);
+        exit(EXIT_FAILURE);
+    }
+
+    char line[256];
+    while (fgets(line, sizeof(line), fp) != NULL) {
+        if (strlen(line) > 1 && line[strlen(line) - 1] == '\n') {
+            line[strlen(line) - 1] = '\0';
+        }
+
+        if (write(fd, line, strlen(line)) == -1) {
+            perror("write");
+            flog(LOG_ERROR, "Failed to write to the pipe\n");
+            break;
+        }
+        flog(LOG_INFO, "Published: %s\n", line);
+        sleep(timeN);
+    }
+
+    close(fd);
+    fclose(fp);
+    flog(LOG_INFO, "Publisher finished\n");
+}
---
+++ src/publisher.h
@@ -0,0 +1,14 @@
+#ifndef PUBLISHER_H
+#define PUBLISHER_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include "flags.h"
+#include "commons.h"
+
+// Function to initialize the publisher
+void start_publisher(int argc, char* argv[]);
+
+#endif
