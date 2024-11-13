9ddd149bfb18bbe0b4fb294dbac863669f7c0c80
Laura Mendieta <laumendieta06@gmail.com>
2024-11-13 12:17:41 -0500

    some details

--- src/subscriber.c
+++ src/subscriber.c
@@ -3,6 +3,7 @@
 void start_subscriber(int argc, char* argv[]) {
     char pipeSSC[255];
 
+    // this helps to read the arguments, same than publishers
     flags_t* parser = flags_create();
     flags_string(parser, pipeSSC, 's', "/tmp/pipeSSC");
 
@@ -11,6 +12,7 @@ void start_subscriber(int argc, char* argv[]) {
 
     flog(LOG_INFO, "Subscriber initialized with pipe: %s\n", pipeSSC);
 
+    // pipe
     int fd = open(pipeSSC, O_RDONLY);
     if (fd == -1) {
         perror("open");
@@ -19,6 +21,7 @@ void start_subscriber(int argc, char* argv[]) {
     }
 
     char buffer[256];
+    // if the buffer is > 0 it took information, if is equal to 0 is the end of the file and if it´s equal to -1 is an error
     while (read(fd, buffer, sizeof(buffer)) > 0) {
         buffer[strcspn(buffer, "\n")] = '\0';
         flog(LOG_INFO, "Received news: %s\n", buffer);
